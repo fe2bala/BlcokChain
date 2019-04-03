4588d10ca39c1f7a321bccd816b1a774f997ab88
Felipe A. Balascio <fbalascio@luxfacta.com.br>
2019-04-03 14:49:52 -0300

    blockchain sem o p2p funcionando

---
+++ Blockchain/Block.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlockchainLib
+{
+    public class Block
+    {
+        private DateTime now;
+        private IList<Transaction> pendingTransactions;
+
+        public int Index { get; set; }
+        public string PreviousHash { get; set; }
+        public IList<Transaction> Transactions { get; set; }
+        public DateTime DateTime { get; set; }
+        public int Nonce { get; set; } = 0;
+
+        public string Hash { get; set; }
+
+        public Block(DateTime dateTime, string previousHash, IList<Transaction> pendingTransactions)
+        {
+            Index = 0;
+            DateTime = dateTime;
+            this.pendingTransactions = pendingTransactions;
+            Hash = CalculateHash();
+        }
+
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{DateTime}-{PreviousHash ?? ""}-{JsonConvert.SerializeObject(Transactions)}-{Nonce}");
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);
+            return Convert.ToBase64String(outputBytes);
+        }
+        public void Mine(int difficulty)
+        {
+            var leadingZeros = new string('0', difficulty);
+            while (this.Hash == null || this.Hash.Substring(0, difficulty) != leadingZeros)
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using BlockchainLib;
+using Newtonsoft.Json;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startTime = DateTime.Now;
+            Blockchain fCoin = new Blockchain();
+            fCoin.CreateTransaction(new Transaction("felipe", "bruno", 10));
+            fCoin.ProcessPendingTransactions("batata");
+            Console.WriteLine(JsonConvert.SerializeObject(fCoin, Formatting.Indented));
+
+            var endTime = DateTime.Now;
+            Console.WriteLine($"Duration: {endTime -startTime}");
+
+            //Console.WriteLine($"FCoin is Valid: {fCoin.IsValid()}");
+            //fCoin.Chain[1].Data = "{sender:Henry,receiver:MaHesh,amount:1000}";
+            //Console.WriteLine($"FCoin is Valid: {fCoin.IsValid()}");
+            Console.ReadKey();
+        }
+    }
+}
